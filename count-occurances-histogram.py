de652c4d4b79e3a1ae15bd00d7dee56f9ae1daf1
ryan-charette <ryanacharette@gmail.com>
2022-04-13 01:43:34 -0500

    Initial commit

---
+++ count-occurances-histogram.py
@@ -0,0 +1,25 @@
+#Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. 
+#The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. 
+#The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. 
+#After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    if line.startswith("From "):
+        words = line.split()
+        email = words[1]
+        counts[email] = counts.get(email, 0) + 1
+    
+modeKey = None
+modeValue = None
+for email,count in counts.items():
+    if modeValue is None or count > modeValue:
+        modeKey = email
+        modeValue = count
+        
+print(modeKey, modeValue)        
